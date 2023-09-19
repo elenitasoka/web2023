c279d1deccb25e87053968080ea028a8262ff250
elenitasoka <elenitasokaa.com>
2023-09-19 19:07:05 +0300

    login runs

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-// login.component.ts
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
@@ -9,21 +9,38 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email = '';
-  password = '';
-  errorMessage = ''; // Error message
 
-  constructor(private authService: AuthService, private router: Router) {}
+  email: string = '';
+  password: string = '';
+
+  isLogin: boolean = true;
+  erroMessage: string = "";
+
+  constructor(private router: Router,private http: HttpClient) {}
 
   login() {
-    this.authService.login(this.email, this.password).subscribe((isLoggedIn) => {
-      if (isLoggedIn) {
-        // If authentication is successful, navigate to the userhome page.
-        this.router.navigateByUrl('/userhome');
-      } else {
-        // If authentication fails, display an error message.
-        this.errorMessage = 'Incorrect Email or Password';
-      }
-    });
+    console.log(this.email);
+    console.log(this.password);
+
+    let bodyData = {
+      email: this.email,
+      password: this.password,
+    };
+
+        this.http.post("http://localhost:9992/users/login", bodyData).subscribe(  (resultData: any) => {
+        console.log(resultData);
+
+        if (resultData.status) 
+        {
+           this.router.navigateByUrl('/userhome');
+    
+        } 
+        else
+         {
+          alert("Incorrect Email or Password");
+          console.log("Errror login");
+        }
+      });
+    } 
   }
-}
+
