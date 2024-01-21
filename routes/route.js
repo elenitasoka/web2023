2d74d7acc463f56844410c9ccc474304558de402
elenitasoka <elenitasokaa.com>
2024-01-21 21:06:12 +0200

    some changes (not sure)

--- routes/route.js
+++ routes/route.js
@@ -5,6 +5,7 @@ const router=express.Router();
 
 router.route('/users/login').post(userController.loginUserControllerFn);
 router.route('/users/create').post(userController.createUserControllerFn);
+router.route('/users/info').get(userController.getUserDataControllerFn);
 
 module.exports=router;
 
