c55f13218fb649663356b5a952ef8edeeef9f39f
elenitasoka <elenitasokaa.com>
2024-02-02 03:05:45 +0200

    .

--- frontend/src/app/userhome/userhome.component.ts
+++ frontend/src/app/userhome/userhome.component.ts
@@ -25,13 +25,8 @@ export class UserhomeComponent {
   showRequestsAndOffers: boolean = false;
   
   showData(){ //sunarthsh gia na emfanisei ta stoixeia tou xrhsth sto console
-<<<<<<< HEAD
-    //console.log(this.logindata.filteredUsers);
-    console.log(this.logindata.filteredUsers[0]);
-=======
     console.log("edwpera:",this.loginDataService.filteredUsers);
   
->>>>>>> 9d2170197ed252d519772cb760d9afa44665a19f
   } 
   
   ngOnInit() : void{
